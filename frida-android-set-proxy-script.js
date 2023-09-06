f649806244b3c0280a1c02666baaa0b4fd530503
Tim Perry <pimterry@gmail.com>
2023-09-06 19:35:49 +0200

    PoC for working Android proxy redirection
    Tried a few different routes, this seems most promising - works fairly
    quickly (~100ms, measured from the remote client), and should be
    reliable for all plausible implementations of ProxySelector, which
    should cover both default & proxy-overriding implementations.
    
    Definitely needs a configurable route (most likely we'll go through
    Android properties here, I think? There's a few possiblities) and this
    notably doesn't handle Flutter, which ignores all Java's proper APIs for
    this (but I think I have some plans for that...)

---
+++ frida-android-set-proxy-script.js
@@ -0,0 +1,42 @@
+// Default emulator address for now:
+const PROXY_HOST = '192.168.104.248';
+const PROXY_PORT = 8000;
+
+setTimeout(function () {
+    Java.perform(function () {
+        const ArrayList = Java.use('java.util.ArrayList');
+        const ProxyType = Java.use('java.net.Proxy$Type');
+        const InetSocketAddress = Java.use('java.net.InetSocketAddress');
+        const ProxyCls = Java.use('java.net.Proxy'); // 'Proxy' is reserved in JS
+
+        const targetProxy = ProxyCls.$new(
+            ProxyType.HTTP.value,
+            InetSocketAddress.$new(PROXY_HOST, PROXY_PORT)
+        );
+
+        const ProxySelector = Java.use('java.net.ProxySelector');
+
+        // Find every implementation of ProxySelector by quickly scanning method signatures, and
+        // then checking whether each match actually implements java.net.ProxySelector:
+        const proxySelectorClasses = Java.enumerateMethods('*!select(java.net.URI): java.util.List/s')
+            .flatMap((matchingLoader) => matchingLoader.classes
+                .map((classData) => Java.use(classData.name))
+                .filter((Cls) => ProxySelector.class.isAssignableFrom(Cls.class))
+            );
+
+        // Replace the 'select' of every implementation, so they all send traffic to us:
+        proxySelectorClasses.forEach(ProxySelectorCls => {
+            console.log('Rewriting', ProxySelectorCls.toString());
+            ProxySelectorCls.select.implementation = () => {
+                const proxyList = ArrayList.$new();
+                proxyList.add(targetProxy);
+                return proxyList;
+            };
+        });
+
+        console.log(
+            `Rewrote ${proxySelectorClasses.length} proxy selector classes ` +
+            `to send traffic to ${PROXY_HOST}:${PROXY_PORT}`
+        );
+    });
+});
