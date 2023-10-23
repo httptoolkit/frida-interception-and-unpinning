88d3796893a5380eba7f2e1844b0eff225912021
Tim Perry <pimterry@gmail.com>
2023-10-23 18:10:10 +0200

    Leave a placeholder frida-script.js script to avoid broken URLs

---
+++ frida-script.js
@@ -0,0 +1,20 @@
+// Placeholder script - this will be deleted soon, it exists purely because the
+// original script URL was popular and still gets many direct visits.
+
+throw new Error(`
+
+    This frida-script.js script has now been replaced with a significantly
+    upgraded set of scripts.
+
+    To upgrade to this new version, please visit the GitHub repo at:
+    https://github.com/httptoolkit/frida-interception-and-unpinning/
+
+    If you'd still like to use the original script, the final version is
+    available via GitHub here:
+    https://github.com/httptoolkit/frida-interception-and-unpinning/tree/4d477da
+
+    This will not be maintained in future, and is already missing many unpinning
+    targets and other features from the latest version. Please don't file issues
+    about any bugs or problems with this script.
+
+`);
